584c9f79ea822eaaf0ae4c6b1348098ed2a8317f
Ankush Puri <ashi9198@gmail.com>
2023-07-02 13:37:05 +0530

    chapter-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,44 +1,96 @@
 import { ResturentCards } from "./ResturentCard";
 import { restrautListData } from "../utils/mokeData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const AppBody = () => {
-  const [restrautList, setRestrautList] = useState(restrautListData);
+  console.log("render");
+  const [restrautList, setRestrautList] = useState([]);
+  const [searchText, setsearchText] = useState("");
+  const [filterResList, setfilterResList] = useState([]);
 
+  // Top Rated filter
   function filterResData() {
     const filterRes = restrautList.filter((res) => {
       return res.data.avgRating > 4;
     });
 
-    setRestrautList(filterRes);
+    setfilterResList(filterRes);
   }
+
+  // Rest Filters
   function reSetFilter() {
-    console.log(restrautListData);
-    setRestrautList(restrautListData);
+    setfilterResList(restrautList);
+    setsearchText("");
+  }
+
+  //Featch Data from API
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3866358&lng=76.3420206&page_type=DESKTOP_WEB_LISTING"
+    );
+    const resList = await data.json();
+
+    setRestrautList(resList?.data?.cards[0]?.data?.data?.cards);
+    setfilterResList(resList?.data?.cards[0]?.data?.data?.cards);
+  };
+
+  // UseEffect to fetch data.
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Search Filter..
+  function searchFilter() {
+    const filterData = restrautList.filter((resturent) => {
+      return resturent?.data?.name
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
+    });
+    console.log(filterData);
+    setfilterResList(filterData);
   }
 
   return (
     <>
-      <div>
-        <button
-          onClick={() => {
-            filterResData();
-          }}
-        >
-          Top reated resturent
-        </button>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            reSetFilter();
-          }}
-        >
-          Reset
-        </button>
+      <div className="filter-container">
+        <div className="search-filter">
+          <input
+            name="Search"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log("ffhgfgh");
+              searchFilter();
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="top-rated-filter">
+          <button
+            onClick={() => {
+              filterResData();
+            }}
+          >
+            Top reated resturent
+          </button>
+        </div>
+        <div className="filter-reset">
+          <button
+            onClick={() => {
+              reSetFilter();
+            }}
+          >
+            Reset
+          </button>
+        </div>
       </div>
       <div className="resturentList">
-        {restrautList.map((resturent) => {
+        {filterResList.map((resturent) => {
           return <ResturentCards {...resturent.data} key={resturent.data.id} />;
         })}
       </div>
